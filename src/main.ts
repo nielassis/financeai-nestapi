aeb07d7e686bbdafb74f90b862bb5ae624e65d8f
nielassis <nielvitorba@gmail.com>
2024-11-15 16:41:06 -0300

    feat: enable cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as helmet from 'helmet';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.enableCors({
+    origin: 'https://financeai-nestapi.vercel.app/',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+  app.use(helmet.default());
   await app.listen(process.env.PORT ?? 3005);
 }
 bootstrap();
